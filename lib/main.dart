0281a87d322849ebb29efa4b7e55f39a94c29a6e
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-08-28 17:06:19 +0900

    Create todolist state

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,26 @@ void main() {
   runApp(const TodoApp());
 }
 
+// https://daily-dev-tips.com/posts/build-a-todo-list-app-with-flutter/
 class TodoApp extends StatelessWidget {
   const TodoApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // app layout
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text("To-do List"),
-      ),
-    ));
+    return MaterialApp(title: 'To-Do List', home: TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TodoList')),
+    );
   }
 }
