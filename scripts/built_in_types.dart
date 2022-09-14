5466d8b309bb57cc2a3aa90085bb0cd83510feba
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-09-14 10:24:07 +0900

    Add built_in_functions

---
+++ scripts/built_in_types.dart
@@ -0,0 +1,37 @@
+// String/Int
+// can't use const here cuz const is a compiletime constant
+final one = int.parse('1');
+final onePointOne = double.parse('1.1');
+final String numAsString = one.toString();
+
+// List(ordered list)
+const list = [1, 2, 3];
+
+// Sets(unordered list)
+const set = {1, 2, 3};
+
+// Maps
+const gifts = {
+  'first': 'partridge',
+  'second': 'turtledoves',
+  'fifth': 'golden rings'
+};
+
+// https://stackoverflow.com/questions/643694/what-is-the-difference-between-utf-8-and-unicode
+
+// functions
+bool returnsBool(bool v) {
+  return v;
+}
+
+int namedParams({required int v1, required int v2}) => v1 + v2;
+
+void main() {
+  print(one);
+  print(numAsString);
+  print(list);
+  print(set);
+  print(gifts);
+  returnsBool(true);
+  print(namedParams(v1: 2, v2: 2));
+}
