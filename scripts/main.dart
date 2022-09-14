2e46c8fcea8df786e8b252abc98effb1a00d5393
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-09-14 09:43:59 +0900

    Add variables

---
+++ scripts/main.dart
@@ -0,0 +1,10 @@
+import './variables.dart';
+
+void printInt(int num) {
+  print(num);
+}
+
+void main() {
+  printInt(2);
+  print(name1);
+}
---
+++ scripts/variables.dart
@@ -0,0 +1,18 @@
+// Basic declaration of variables
+var name1 = 'bob';
+String name2 = 'bob2';
+
+// Default Values
+int? count;
+
+// Final and Const
+// final: runtime constant
+// const: compiletime constant
+
+final name3 = 'BOBBU';
+// name3 = 's'; // Error: A value can't be assigned to a final variable.
+const name4 = "Alice";
+
+void main() {
+  print(count); // => null
+}
