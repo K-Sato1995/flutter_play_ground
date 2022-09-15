9de2c6238bbfcf9a52d7a1a2a05ec42267748a90
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-09-15 19:20:18 +0900

    Add text colo

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('test', textDirection: TextDirection.ltr));
+    return const Center(
+        child: Text('test',
+            style: TextStyle(color: Color(0xFFFFFFFF)),
+            textDirection: TextDirection.ltr));
   }
 }
