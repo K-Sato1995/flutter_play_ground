d199a7183436d84d83b19baa1fa5cf0342af4cdf
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-09-19 19:16:45 +0900

    Tmp

--- lib/fetch.dart
+++ lib/fetch.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class FetchExample extends StatelessWidget {
+class FetchExample extends StatefulWidget {
   const FetchExample({super.key});
 
   @override
+  State<FetchExample> createState() => _FetchExampleState();
+}
+
+class _FetchExampleState extends State<FetchExample> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Container(Text(futureAlbum.data.title));
+  }
+}
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({this.userId, this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
   }
 }
