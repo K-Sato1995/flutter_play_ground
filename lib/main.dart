c6b49b67dbed2f6fd3144eeb89b6877312d07976
K-Sato1995 <katsuki_sato0130@icloud.com>
2022-09-20 15:33:38 +0900

    Simplest Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/Todo.dart';
 
 void main() {
-  runApp(const Main());
+  runApp(const MyApp());
 }
 
 // Row: Vertical Layout
 // Column: Horizontal Layout
 // Container: Separate widgets using padding, or to add borders or margins
-class Main extends StatelessWidget {
-  const Main({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,46 @@ class Main extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(appBar: AppBar(title: Text("My Todo App")), body: Todo()),
+      home: const Page1(),
     );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Page 1")),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Page 1', style: TextStyle(fontSize: 30)),
+            ElevatedButton(
+                onPressed: () => {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Page2()),
+                      )
+                    },
+                child: Text('Go to Page 2'))
+          ],
+        )));
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Page 2"),
+        ),
+        body: Center(
+          child: Text('Secod page'),
+        ));
   }
 }
